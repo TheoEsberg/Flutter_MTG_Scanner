3a8a389f3f072b008d7556e5c5f0e435fe8966bc
Theo Esberg <esbergtheo@gmail.com>
2024-04-16 16:54:32 +0200

    [#187440702] - Made some changes that gave access to the camera and the ability to take pictures.

--- lib/features/scan/views/scan_view.dart
+++ lib/features/scan/views/scan_view.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mtg_scanner/features/scan/widgets/camera_widget.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ScanView extends ConsumerWidget {
@@ -6,8 +10,54 @@ class ScanView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Center(
-      child: Text('Scan View!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('MTG Scanner'),
+        ),
+      ),
+      body: FutureBuilder<CameraDescription>(
+        future: getCamera(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              return CameraWidget(camera: snapshot.data!);
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              return const Text('Waiting for camera...');
+            }
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      ),
+    );
+  }
+}
+
+Future<CameraDescription> getCamera() async {
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+  return firstCamera;
+}
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Display the Picture')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Center(
+        child: Image.file(
+          File(imagePath),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,9 @@ import 'package:flutter_mtg_scanner/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
+  // The [WidgetsFlutterBinding] is used to ensure
+  // that plugin services are correctly initialized.
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const ProviderScope(child: MyApp()));
 }
